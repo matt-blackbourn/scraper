213e25b4d7eb6431405f9d88a518b96a3cd3c74c
matt-blackbourn <matt.blackie114@gmail.come>
2022-08-09 18:01:31 +1200

    add node-cron...working

--- work.ts
+++ work.ts
@@ -1,10 +1,13 @@
 import { DataFetcher } from './DataFetcher'
+const cron = require('node-cron')
 
 const companies: Array<string> = ['CHI', 'CNU', 'IFT', 'VCT', 'PPH', 'EBO']
 const url = process.env.URL
 
 const fetcher = new DataFetcher()
 
-fetcher.getData(url, companies)
-    .then(res => console.table(res))
-    .catch(error => console.log(error))
+cron.schedule ('* * * * *', () => {
+    fetcher.getData(url, companies)
+        .then(res => console.table(res))
+        .catch(error => console.log(error))
+})
